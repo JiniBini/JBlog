8c2defe810274150b6482272f20821bd7797ad58
EunJin <julietjin94@gmail.com>
2020-09-20 20:44:52 +0900

    basic shape_change App.js

--- react_jblog/src/App.js
+++ react_jblog/src/App.js
@@ -4,21 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Head">
+      <body>
+        <p>여기는 은빈이가 연습할 곳 html 구조 보이지? div, body, p 지금 간단한데 다양하게 변화할 수 있지! </p>
+      </body>
     </div>
   );
 }
