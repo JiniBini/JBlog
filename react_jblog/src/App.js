4b2920a36a55a5ac12061990533eb007eaad6eb6
EunJin <julietjin94@gmail.com>
2021-01-19 16:28:41 +0900

    prepare web main

--- react_jblog/src/App.js
+++ react_jblog/src/App.js
@@ -5,8 +5,13 @@ function App() {
   return (
     <div className="Head">
       <body>
-        <p>여기는 은빈이가 연습할 곳. html이랑 비슷하지만, 코드 보면 function 안에 있는 걸 알 수 있어..!</p>
-        <p><hr color="black" width="90%"/></p>
+        <div>
+          menu
+        </div>
+        <div>
+          <p>in div</p>
+          <p><hr color="black" width="90%" /></p>
+        </div>
       </body>
     </div>
   );
